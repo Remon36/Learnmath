e536744b03e211f5e01e5f4dd219f78bb4ed591e
remonwilliam3 <58605696+remonwilliam3@users.noreply.github.com>
2019-12-18 12:29:53 +0200

    Add files via upload

--- remon.js
+++ remon.js
@@ -1,58 +1,58 @@
-<script>]
+<script>
 function generate(){
 	var counter_correct=0;
 	var counter_wrong=0;
+	var q=new Array[100];
+	var a=new Array[100];
+	var c1=new Array[100];
+	var c2=new Array[100];
+	var c3=new Array[100];
+	var c4=new Array[100];
 for(var i=1;i<=100;i++){
 var x=Math.floor(1+Math.random()*10000);
 var y=Math.floor(1+Math.random()*10000);
-var q+"i";
 var z=Math.floor(1+Math.random()*4);
 if(z==1){
-	q+"i"=x+y;
-document.getElementById(question+"i").innerHTML=x+"+"+y+"=";
+	x[i]=x+"+"+y+"=";
+	a[i]=x+y;
+	c1[i]=a[i];
+	c2[i]=a[i]+x;
+	c3[i]=a[i]+y;
+    c4[i]=a[i]+x+y
 }
 else if(z==2){
 	if(x>y){
-		document.getElementById(question+"i").innerHTML=x+"-"+y+"=";
-		var answer = parseInt(document.getElementById(answer+"i").value);
-		q+"i"=x-y;
-		if(q+"i"==answer){
-			counter_correct++;
-		}
-		else{
-			counter_wrong++;
-		}
+	x[i]=x+"-"+y+"=";
+	a[i]=x-y;
+	c1[i]=a[i];
+	c2[i]=a[i]+x;
+	c3[i]=a[i]+y;
+    c4[i]=a[i]+x+y
 	}
 	else{
-		var answer = parseInt(document.getElementById(question+"i").innerHTML=x"-"y+"=";
-		q+"i"=y-x;
-		if(q+"i"==answer){
-			counter_correct++;
-		}
-		else{
-			counter_wrong++;
-		}
+	x[i]=y+"-"+x+"=";
+	a[i]=y-x;
+	c1[i]=a[i];
+	c2[i]=a[i]+x;
+	c3[i]=a[i]+y;
+    c4[i]=a[i]+x+y
 	}
 }
 else if(z==3){
-	var answer = parseInt(document.getElementById(question+"i").innerHTML=x+"*"y+"=";
-		q+"i"=x*y;
-		if(q+"i"==answer){
-			counter_correct++;
-		}
-		else{
-			counter_wrong++;
-		}
+	x[i]=x+"*"+y+"=";
+	a[i]=x*y;
+	c1[i]=a[i];
+	c2[i]=a[i]+x;
+	c3[i]=a[i]+y;
+    c4[i]=a[i]+x+y
 }
 else{
-	var answer = parseInt(document.getElementById(question+"i").innerHTML=x+"/"y+"=";
-		q+"i"=x/y;
-		if(q+"i"==answer){
-			counter_correct++;
-		}
-		else{
-			counter_wrong++;
-		}
+	x[i]=x+"/"+y+"=";
+	a[i]=x/y;
+	c1[i]=a[i];
+	c2[i]=a[i]+x;
+	c3[i]=a[i]+y;
+    c4[i]=a[i]+x+y
 }
 }
 }
